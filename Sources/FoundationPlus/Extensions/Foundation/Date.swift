474ec077707a8727f21bfe2016990129f57b4164
Ben Myers <benl.myers01@gmail.com>
2021-12-28 20:03:09 -0500

    🖌 Quick date formatting using style string

--- Sources/FoundationPlus/Extensions/Foundation/Date.swift
+++ Sources/FoundationPlus/Extensions/Foundation/Date.swift
@@ -10,9 +10,24 @@ import Foundation
 @available(iOS 13.0, *)
 public extension Date {
   
-  // MARK: - Static Methods
+  // MARK: - Public Static Methods
   
   static func - (lhs: Date, rhs: Date) -> TimeInterval {
     return rhs.distance(to: lhs)
+  }
+  
+  // MARK: - Public Methods
+  
+  /**
+   Formats the date.
+   
+   - parameter style: The format style, i.e. "h:MM"
+   */
+  func formatted(_ style: String, _ am: String = "AM", _ pm: String = "PM") -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = style
+    formatter.amSymbol = am
+    formatter.pmSymbol = pm
+    return formatter.string(from: self)
   }
 }
